3933ee0770a6e646f05da64581456fceeafcc4c9
josedlca <jose.dlcruz.ayala@gmail.com>
2021-02-10 14:17:25 -0500

    search and call api

--- src/App.js
+++ src/App.js
@@ -6,24 +6,38 @@ class App extends Component{
   constructor(){
     super()
     this.state={
-      
+      API_KEY:'AIzaSyDx4hKw7GYUajee_cZzcpuId8V8FvnKizI',
+      searchInput: ""
     }
   }
-  componentDidMount(){
-    const API_KEY = 'AIzaSyDx4hKw7GYUajee_cZzcpuId8V8FvnKizI';
-  
-    const api = new YoutubeDataAPI(API_KEY);
-    
-    api.searchAll("Pandas",25).then((data) => {
+
+  handleClick=()=>{
+    const api = new YoutubeDataAPI(this.state.API_KEY);
+    api.searchAll(this.state.searchInput,25).then((data) => {
         console.log(data);
     },(err) => {
         console.error(err);
     })
   }
+
+  handleChangeInput=(e)=>{
+    const {name,value} = e.target
+    this.setState({
+      [name]:value
+    })
+  }
+
   render(){
     return (
       <div className="App">
-        Hello Youtube
+        <input 
+          type="text"
+          value={this.state.searchInput}
+          name="searchInput"
+          placeholder="Buscar"
+          onChange={this.handleChangeInput}
+        />
+        <button onClick={this.handleClick}>Buscar</button>
       </div>
     );
   }

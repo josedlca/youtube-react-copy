f4f081c3194137bee064f85663e3743ec8b128a6
josedlca <jose.dlcruz.ayala@gmail.com>
2021-02-10 11:28:36 -0500

    adding youtubev3 api calls

--- src/App.js
+++ src/App.js
@@ -1,12 +1,32 @@
-import React from 'react'
+import React,{Component} from 'react'
 import './App.css';
+import {YoutubeDataAPI} from 'youtube-v3-api'
 
-function App() {
-  return (
-    <div className="App">
-      Hello Youtube
-    </div>
-  );
+class App extends Component{
+  constructor(){
+    super()
+    this.state={
+      
+    }
+  }
+  componentDidMount(){
+    const API_KEY = 'AIzaSyDx4hKw7GYUajee_cZzcpuId8V8FvnKizI';
+  
+    const api = new YoutubeDataAPI(API_KEY);
+    
+    api.searchAll("Pandas",25).then((data) => {
+        console.log(data);
+    },(err) => {
+        console.error(err);
+    })
+  }
+  render(){
+    return (
+      <div className="App">
+        Hello Youtube
+      </div>
+    );
+  }
 }
 
 export default App;
